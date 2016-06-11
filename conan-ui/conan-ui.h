b66fdb3e7678925133ddc085feb0a698ff19cb1f
Austin Felipe Santos <austinfelipe@ig.com.br>
2016-06-11 11:46:22 -0300

    Initial Commit

---
+++ conan-ui/conan-ui.h
@@ -0,0 +1,19 @@
+//
+//  conan-ui.h
+//  conan-ui
+//
+//  Created by Austin Felipe on 2016-06-11.
+//  Copyright © 2016 Austin Felipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for conan-ui.
+FOUNDATION_EXPORT double conan_uiVersionNumber;
+
+//! Project version string for conan-ui.
+FOUNDATION_EXPORT const unsigned char conan_uiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <conan_ui/PublicHeader.h>
+
+
